ae35b7d45b9057fdcb2f4944ab24440d94735df8
Victor Pan <victorpan2017@gmail.com>
2022-02-20 18:00:47 -0800

    Implemented AddressBook

--- src/address/data/AddressBook.java
+++ src/address/data/AddressBook.java
@@ -1,4 +1,137 @@
 package address.data;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * The {@link AddressBook} contains a collection of {@link AddressEntry address entries}.
+ */
 public class AddressBook {
+
+    /**
+     * A collection of {@link AddressEntry address entries}, sorted by last name.
+     */
+    private SortedSet<AddressEntry> entries;
+
+    /**
+     * Compares {@link AddressEntry address entries} by last name, then first name.
+     */
+    public final class AddressEntryComparator implements Comparator<AddressEntry> {
+
+        /**
+         * @param entry1 The first {@link AddressEntry}.
+         * @param entry2 The second {@link AddressEntry}.
+         * @return <l>
+         * <li> &#8804; -1 if entry1 is less than entry2.</li>
+         * <li>0 if entry1 and entry2 are equal.</li>
+         * <li> &#8805; 1 if entry1 is greater than entry2.</li>
+         * </l>
+         */
+        @Override
+        public int compare(AddressEntry entry1, AddressEntry entry2) {
+            if (entry1 == null && entry2 == null) {
+                return 0;
+            }
+            if (entry1 == null && entry2 != null) {
+                return -1;
+            }
+            if (entry1 != null && entry2 == null) {
+                return 1;
+            }
+
+            if (entry1.getLastName() == null && entry2.getLastName() == null) {
+                return 0;
+            }
+            if (entry1.getLastName() == null && entry2.getLastName() != null) {
+                return -1;
+            }
+
+            int comp = entry1.getLastName().compareTo(entry2.getLastName());
+            if (comp == 0) {
+                if (entry1.getFirstName() == null && entry2.getFirstName() == null) {
+                    return 0;
+                }
+                if (entry1.getFirstName() == null && entry2.getFirstName() != null) {
+                    return -1;
+                }
+                return entry1.getFirstName().compareTo(entry2.getFirstName());
+            }
+            return comp;
+        }
+    }
+
+    /**
+     * Initializes an empty address book.
+     */
+    public AddressBook() {
+        entries = new TreeSet<>(new AddressEntryComparator());
+    }
+
+    /**
+     * Returns the number of {@link AddressEntry address entries} in the {@link AddressBook}.
+     *
+     * @return the number of elements in this {@link AddressBook}.
+     */
+    public int size() {
+        return entries.size();
+    }
+
+    public void loadFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String firstName = scanner.nextLine();
+                String lastName = scanner.nextLine();
+                String street = scanner.nextLine();
+                String city = scanner.nextLine();
+                String state = scanner.nextLine();
+                String zip = scanner.nextLine();
+                String email = scanner.nextLine();
+                String phone = scanner.nextLine();
+
+                addEntry(new AddressEntry(firstName, lastName, street, city, state, Integer.parseInt(zip), email, phone));
+            }
+            scanner.close();
+        } catch (IOException e) {
+            System.out.println("Could not read from file at: " + filePath);
+        }
+    }
+
+    public void addEntry(AddressEntry entry) {
+        entries.add(entry);
+    }
+
+    public boolean removeEntry(AddressEntry entry) {
+        return entries.remove(entry);
+    }
+
+    public List<AddressEntry> findEntries(String lastName) {
+        return entries.stream()
+                .filter(e -> e.getLastName().startsWith(lastName))
+                .toList();
+    }
+
+    public void listEntries() {
+        int i = 1;
+        StringBuilder stringBuilder = new StringBuilder(255);
+        for (AddressEntry entry : entries) {
+            stringBuilder
+                    .append(i).append(": ").append(entry.getFirstName()).append(' ').append(entry.getLastName())
+                    .append('\n')
+                    .append(entry.getStreet())
+                    .append('\n')
+                    .append(entry.getCity()).append(", ").append(entry.getState()).append(' ').append(entry.getZip())
+                    .append('\n')
+                    .append(entry.getPhone())
+                    .append('\n')
+                    .append(entry.getEmail())
+                    .append('\n');
+
+            System.out.println(stringBuilder.toString());
+            stringBuilder.delete(0, stringBuilder.length());
+            i++;
+        }
+    }
 }
