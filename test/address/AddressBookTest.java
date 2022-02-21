e6ee0985b9cf848f961a08183a36497cead4385f
Victor Pan <victorpan2017@gmail.com>
2022-02-20 17:28:37 -0800

    Fix AddressBook test cases

--- test/address/AddressBookTest.java
+++ test/address/AddressBookTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 import java.util.Optional;
 
@@ -99,35 +100,42 @@ public class AddressBookTest {
         entries = ab.findEntries(this.entry.getLastName());
         Assertions.assertEquals(2, entries.size());
         Assertions.assertTrue(entries.contains(newEntry));
-        Assertions.assertTrue(entries.contains(entry));
+        Assertions.assertTrue(entries.contains(entry.get()));
     }
 
     @Test
     void listEntries() {
 
         Assertions.assertDoesNotThrow(() -> {
-            BufferedOutputStream stream = new BufferedOutputStream(System.out);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            PrintStream old = System.out;
+            System.setOut(new PrintStream(stream));
 
             ab.listEntries();
 
-            String list = "1. Victor Pan\n12345 Street, City, California, 69420\n555-420-6969\nvpan2@horizon.csueastbay.edu";
-            Assertions.assertEquals(list, stream.toString());
+            System.out.flush();
+            System.setOut(old);
 
-            stream.close();
+            String list = "1: Victor Pan\n12345 Street\nCity, California 69420\n555-420-6969\nvpan2@horizon.csueastbay.edu\n\r\n";
+            Assertions.assertEquals(list, stream.toString());
         });
 
         AddressEntry newEntry = new AddressEntry("Emily", "Mars", "987 Street", "City", "California", 42069, "555-123-6942", "email@test.com");
         ab.addEntry(newEntry);
 
         Assertions.assertDoesNotThrow(() -> {
-            BufferedOutputStream stream = new BufferedOutputStream(System.out);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            PrintStream old = System.out;
+            System.setOut(new PrintStream(stream));
 
             ab.listEntries();
 
-            String list = "1. Emily Mars\n987 Street\nCity, California, 42069\n555-123-6942\nemail@test.com\n\n2. Victor Pan\n12345 Street\nCity, California, 69420\n555-420-6969\nvpan2@horizon.csueastbay.edu\n";
+            System.out.flush();
+            System.setOut(old);
+
+            String list = "1: Emily Mars\n987 Street\nCity, California 42069\n555-123-6942\nemail@test.com\n\r\n2: Victor Pan\n12345 Street\nCity, California 69420\n555-420-6969\nvpan2@horizon.csueastbay.edu\n\r\n";
             Assertions.assertEquals(list, stream.toString());
 
-            stream.close();
         });
     }
 }
