a9a02300b751039108d27506be1d7060732f8bb2
Victor Pan <victorpan2017@gmail.com>
2022-02-20 23:50:21 -0800

    Update AddressBookApplication.java

--- src/address/AddressBookApplication.java
+++ src/address/AddressBookApplication.java
@@ -1,8 +1,43 @@
 package address;
 
+import address.data.AddressBook;
+import address.data.AddressEntry;
+
+/**
+ * AddressBookApplication is the entry point of the application and
+ * calls some of the {@link Menu} class methods.
+ */
 public class AddressBookApplication {
 
+    /**
+     * The entry point of the {@link AddressBookApplication} and calls
+     * some of the {@link Menu} class methods.
+     *
+     * @param args The input arguments.
+     */
     public static void main(String[] args) {
-	// write your code here
+        final String filePath = "resources/AddressInputDataFile.txt";
+
+        AddressBook ab = new AddressBook();
+        initAddressBookApplication(ab, filePath);
+
+        Menu menu = new Menu();
+        boolean shouldQuit;
+
+        do {
+            shouldQuit = menu.promptMenu(ab);
+
+        } while (!shouldQuit);
+    }
+
+    /**
+     * Initializes the {@link AddressBook} ab from a text file.
+     *
+     * @param ab       The {@link AddressBook} that will have two
+     *                 {@link AddressEntry address entries} added to it.
+     * @param filePath The path to the text file to load {@link AddressEntry address entries} from.
+     */
+    private static void initAddressBookApplication(AddressBook ab, String filePath) {
+        ab.loadFile(filePath);
     }
 }
