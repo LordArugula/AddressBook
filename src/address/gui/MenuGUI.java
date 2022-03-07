a907270b98c14de7a8147e744cfc12fa886a6140
Victor Pan <victorpan2017@gmail.com>
2022-03-06 21:58:09 -0800

    Create initGUI method in MenuGUI

--- src/address/gui/MenuGUI.java
+++ src/address/gui/MenuGUI.java
@@ -9,3 +9,30 @@ public class MenuGUI extends JFrame {
     private JButton removeButton;
     private JScrollPane scrollPane;
     private JPanel entryFormContainer;
+    private AddressBook ab;
+
+    public MenuGUI(AddressBook ab) {
+        this.ab = ab;
+
+        initUI();
+    }
+
+    private void initUI() {
+        setTitle("Address Book Application");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(640, 480);
+        setLocationRelativeTo(null);
+        setDefaultLookAndFeelDecorated(false);
+
+        add(root);
+    }
+
+    public AddressBook getAddressBook() {
+        return ab;
+    }
+
+    public void setAddressBook(AddressBook ab) {
+        this.ab = ab;
+    }
+}
