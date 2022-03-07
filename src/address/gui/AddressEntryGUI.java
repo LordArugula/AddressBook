3a91335e150a366d3090e9adb9655568ec817c8c
Victor Pan <victorpan2017@gmail.com>
2022-03-06 21:56:23 -0800

    Created MenuGUI and AddressEntryGUI forms

---
+++ src/address/gui/AddressEntryGUI.java
@@ -0,0 +1,16 @@
+package address.gui;
+
+import javax.swing.*;
+
+public class AddressEntryGUI {
+    private JTextField firstNameInput;
+    private JTextField lastNameInput;
+    private JTextField streetInput;
+    private JTextField cityInput;
+    private JTextField stateInput;
+    private JTextField zipInput;
+    private JTextField phoneInput;
+    private JTextField emailInput;
+    private JPanel root;
+    private JButton submitButton;
+}
---
+++ src/address/gui/MenuGUI.java
@@ -0,0 +1,11 @@
+package address.gui;
+
+import javax.swing.*;
+
+public class MenuGUI extends JFrame {
+    private JPanel root;
+    private JButton displayButton;
+    private JButton newButton;
+    private JButton removeButton;
+    private JScrollPane scrollPane;
+    private JPanel entryFormContainer;
