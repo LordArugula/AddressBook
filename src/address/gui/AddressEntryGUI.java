463eafc0fd19be93b09aa97da1580fc8c25abf51
Victor Pan <victorpan2017@gmail.com>
2022-03-06 22:00:31 -0800

    Added javadoc comments for MenuGUI and AddressEntryGUI

--- src/address/gui/AddressEntryGUI.java
+++ src/address/gui/AddressEntryGUI.java
@@ -1,16 +1,61 @@
 package address.gui;
 
+import address.data.AddressEntry;
+
 import javax.swing.*;
 
+/**
+ * Defines the GUI for creating and editing {@link AddressEntry address entries}.
+ */
 public class AddressEntryGUI {
-    private JTextField firstNameInput;
-    private JTextField lastNameInput;
-    private JTextField streetInput;
-    private JTextField cityInput;
-    private JTextField stateInput;
-    private JTextField zipInput;
-    private JTextField phoneInput;
-    private JTextField emailInput;
+
+    /**
+     * The root panel.
+     */
     private JPanel root;
+
+    /**
+     * The text field for the first name.
+     */
+    private JTextField firstNameInput;
+
+    /**
+     * The text field for the last name.
+     */
+    private JTextField lastNameInput;
+
+    /**
+     * The text field for the last name.
+     */
+    private JTextField streetInput;
+
+    /**
+     * The text field for the last name.
+     */
+    private JTextField cityInput;
+
+    /**
+     * The text field for the last name.
+     */
+    private JTextField stateInput;
+
+    /**
+     * The text field for the last name.
+     */
+    private JTextField zipInput;
+
+    /**
+     * The text field for the last name.
+     */
+    private JTextField phoneInput;
+
+    /**
+     * The text field for the last name.
+     */
+    private JTextField emailInput;
+
+    /**
+     * The button to submit the {@link AddressEntry}.
+     */
     private JButton submitButton;
 }
--- src/address/gui/MenuGUI.java
+++ src/address/gui/MenuGUI.java
@@ -1,22 +1,64 @@
 package address.gui;
 
+import address.AddressBookApplication;
+import address.data.AddressBook;
+import address.data.AddressEntry;
+
 import javax.swing.*;
 
+/**
+ * Defines the GUI for the {@link AddressBookApplication}.
+ */
 public class MenuGUI extends JFrame {
+    /**
+     * The root panel.
+     */
     private JPanel root;
+
+    /**
+     * The button to display {@link AddressEntry address entries}.
+     */
     private JButton displayButton;
+
+    /**
+     * The button to add new {@link AddressEntry address entries}.
+     */
     private JButton newButton;
+
+    /**
+     * The button to remove {@link AddressEntry address entries}.
+     */
     private JButton removeButton;
+
+    /**
+     * The scroll pane to view {@link AddressEntry address entries}.
+     */
     private JScrollPane scrollPane;
+
+    /**
+     * The panel to contain the address entry form.
+     * This is shown when adding a new {@link AddressEntry}.
+     */
     private JPanel entryFormContainer;
+
+    /**
+     * The {@link AddressBook}.
+     */
     private AddressBook ab;
 
+    /**
+     * Initializes a {@link MenuGUI} with an {@link AddressBook}.
+     * @param ab The {@link AddressBook} to use with the {@link MenuGUI}.
+     */
     public MenuGUI(AddressBook ab) {
         this.ab = ab;
 
         initUI();
     }
 
+    /**
+     * Initializes the GUI.
+     */
     private void initUI() {
         setTitle("Address Book Application");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -28,10 +70,18 @@ public class MenuGUI extends JFrame {
         add(root);
     }
 
+    /**
+     * Gets the {@link AddressBook}.
+     * @return the {@link AddressBook}.
+     */
     public AddressBook getAddressBook() {
         return ab;
     }
 
+    /**
+     * Sets the {@link AddressBook}.
+     * @param ab The {@link AddressBook}.
+     */
     public void setAddressBook(AddressBook ab) {
         this.ab = ab;
     }
