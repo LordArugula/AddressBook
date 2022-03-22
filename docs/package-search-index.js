93b3217b20a9f2af8a33a00f65c99c933759d884
Victor Pan <victorpan2017@gmail.com>
2022-03-22 00:05:23 -0700

    Update javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 packageSearchIndex = [{"l":"address"},{"l":"address.data"},{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
+=======
+packageSearchIndex = [{"l":"address"},{"l":"address.data"},{"l":"address.gui"},{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
+>>>>>>> Stashed changes
--- docs/tag-search-index.js
+++ docs/tag-search-index.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"}];updateSearchResults();
+=======
+tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"},{"l":"Serialized Form","h":"","u":"serialized-form.html"}];updateSearchResults();
+>>>>>>> Stashed changes
