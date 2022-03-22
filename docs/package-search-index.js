c7c94c2af6e71460831abd773112a8156831d681
Victor Pan <41593388+LordArugula@users.noreply.github.com>
2022-03-22 00:52:41 -0700

    Update generated javadoc html

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"address"},{"l":"address.data"},{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
+packageSearchIndex = [{"l":"address"},{"l":"address.data"},{"l":"address.gui"},{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
